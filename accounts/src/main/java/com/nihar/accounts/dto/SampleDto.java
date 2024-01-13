d06e2b537e3c4f33edcda076cff4cad0bfb31291
Nihar Kanta Tripathy <niharkanta1@live.com>
2024-01-13 11:37:44 +0530

    accounts change

---
+++ accounts/src/main/java/com/nihar/accounts/dto/SampleDto.java
@@ -0,0 +1,12 @@
+package com.nihar.accounts.dto;
+
+import java.io.Serializable;
+
+public class SampleDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6170215135766762077L;
+	
+}
